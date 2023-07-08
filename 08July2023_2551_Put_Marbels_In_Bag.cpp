e1d0cee9bd1b131ef140f498d70a819e56eb46e0
Kunal chawla <73349039+kcchawla85@users.noreply.github.com>
2023-07-08 21:42:51 +0530

    Create 08July2023_2551_Put_Marbels_In_Bag.cpp

---
+++ 08July2023_2551_Put_Marbels_In_Bag.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    long long putMarbles(vector<int>& v, int k) {
+            
+            int n = v.size();
+            
+            long long  mx = v[0] + v[n-1]; // Maximum Value 
+            long long  mn = v[0] + v[n-1]; // Minimum Value
+            
+            vector<long long > adjSum; // For storing adjacent sum
+            
+            for(int i = 0;i<n-1;i++)
+                    adjSum.push_back(v[i] + v[i+1]);  // Storing adjacent sum
+            
+            
+            sort(adjSum.begin(),adjSum.end(),greater<int>()); // Sorting in descending order
+            
+            
+            
+            for(int i = 0;i<k-1;i++)
+                    mx +=(long long) adjSum[i], // adding maximum to mx
+                    mn +=(long long) adjSum[n-i-2]; // adding minimum to mn
+            
+          
+            
+           
+            
+          
+            return (mx- mn); // difference is the answer
+            
+            
+            
+            
+        
+    }
+};
+//link: https://www.youtube.com/watch?v=K0opAL-J07U
+// code link: https://leetcode.com/problems/put-marbles-in-bags/solutions/3735251/intution-building-explanation-c/
