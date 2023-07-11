e4bc60134f6d0bbc8dc9051a281a2b2b2a6e87dc
Kunal chawla <73349039+kcchawla85@users.noreply.github.com>
2023-07-11 22:01:14 +0530

    Create 11July2023_863_All_Nodes_Distance_K_in_Binary_Tree.cpp

---
+++ 11July2023_863_All_Nodes_Distance_K_in_Binary_Tree.cpp
@@ -0,0 +1,56 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    unordered_map<TreeNode*, TreeNode*> mp;
+    void dfs(TreeNode* root, TreeNode* parent){
+        if(!root) return;
+        if(!parent) parent = root;
+        else mp[root] = parent;
+        dfs(root->left, root);
+        dfs(root->right, root);
+    }
+    vector<int> distanceK(TreeNode* root, TreeNode* target, int k) {
+        queue<TreeNode*> q;
+        set<TreeNode*> st;
+        dfs(root,NULL);
+        q.push(target);
+        int cnt=0;
+        vector<int> res;
+        while(!q.empty()){
+            int sz=q.size();
+            if(cnt>k) break;
+            while(sz--){
+                auto node=q.front();
+                q.pop();
+                if(node->left!=NULL and !st.count(node->left)){
+                    q.push(node->left);
+                    st.insert(node->left);
+                }
+                if(node->right!=NULL and !st.count(node->right)){
+                    q.push(node->right);
+                    st.insert(node->right);
+                }
+                if(mp.count(node) and !st.count(mp[node])){
+                    q.push(mp[node]);
+                    st.insert(mp[node]);
+                }
+                st.insert(node);
+                if(cnt==k){
+                    res.push_back(node->val);
+                }
+            }
+            cnt++;
+        }
+        return res;
+    }
+};
+// link : https://leetcode.com/problems/all-nodes-distance-k-in-binary-tree/solutions/3747669/c-faster-than-90-of-sol-n-using-bfs-dfs-store-parent-of-each-node-clean-and-concise/
+// https://www.youtube.com/watch?v=myH6qdThNPY
