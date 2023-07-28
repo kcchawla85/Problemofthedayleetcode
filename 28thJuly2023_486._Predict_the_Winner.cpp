d4fbd0886c2a7e1a383a306ec3daecc080ae5b31
Kunal chawla <73349039+kcchawla85@users.noreply.github.com>
2023-07-28 22:04:32 +0530

    Create 28thJuly2023_486._Predict_the_Winner.cpp

---
+++ 28thJuly2023_486._Predict_the_Winner.cpp
@@ -0,0 +1,28 @@
+class Solution {
+private:
+    vector<vector<int>> dp;
+    
+    int score(vector<int>& nums, int l, int r) {
+        if (dp[l][r] != -1) {
+            return dp[l][r];
+        }
+        if (l == r) {
+            return nums[l];
+        }
+        
+        int left = nums[l] - score(nums, l + 1, r);
+        int right = nums[r] - score(nums, l, r - 1);
+        dp[l][r] = max(left, right);
+        
+        return dp[l][r];
+    }
+    
+public:
+    bool PredictTheWinner(vector<int>& nums) {
+        int n = nums.size();
+        dp = vector<vector<int>>(n, vector<int>(n, -1));
+        
+        return score(nums, 0, n - 1) >= 0;
+    }
+};
+// link : https://leetcode.com/problems/predict-the-winner/solutions/3826440/beats-100-video-java-c-python/
