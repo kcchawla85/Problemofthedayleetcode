225bb3f735906e20abbfae26fce3b36257ab91a9
Kunal chawla <73349039+kcchawla85@users.noreply.github.com>
2023-08-05 22:38:53 +0530

    Create 05August2023_95_Unique_Binary_Search_Trees_II.cpp

---
+++ 05August2023_95_Unique_Binary_Search_Trees_II.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<TreeNode*> generateTrees(int n) {
+        return n ? generate_trees(1, n) : vector<TreeNode*>();
+    }
+
+private:
+    vector<TreeNode*> generate_trees(int start, int end) {
+        if (start > end) return {nullptr};
+
+        vector<TreeNode*> all_trees;
+        for (int i = start; i <= end; i++) {
+            vector<TreeNode*> left_trees = generate_trees(start, i - 1);
+            vector<TreeNode*> right_trees = generate_trees(i + 1, end);
+
+            for (TreeNode* l : left_trees) {
+                for (TreeNode* r : right_trees) {
+                    TreeNode* current_tree = new TreeNode(i);
+                    current_tree->left = l;
+                    current_tree->right = r;
+                    all_trees.push_back(current_tree);
+                }
+            }
+        }
+        return all_trees;
+    }
+};
+// link: https://leetcode.com/problems/unique-binary-search-trees-ii/solutions/3864900/recursion-dp-video-catalan-number-unique-bst/
