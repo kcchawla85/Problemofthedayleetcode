10b3fd2735b37c7f3e93ebc92dbad50d15a109fd
Kunal chawla <73349039+kcchawla85@users.noreply.github.com>
2023-08-22 23:00:54 +0200

    Create 22august2023.cpp

---
+++ 22august2023.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    string convertToTitle(int columnNumber) {
+        string result = "";
+        while (columnNumber) {
+            columnNumber--;
+            char c = 'A' + columnNumber % 26;
+            result = c + result;
+            columnNumber /= 26;
+        }
+        return result;
+    }
+};
