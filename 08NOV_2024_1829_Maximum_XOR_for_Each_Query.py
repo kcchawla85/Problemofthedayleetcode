bd09f7337f4ce09b8fe6184ce7d9ef9ca659a01e
Kunal Chawla <73349039+kcchawla85@users.noreply.github.com>
2024-11-08 21:52:37 +0100

    Create 08NOV_2024_1829_Maximum_XOR_for_Each_Query.py

---
+++ 08NOV_2024_1829_Maximum_XOR_for_Each_Query.py
@@ -0,0 +1,18 @@
+#https://leetcode.com/problems/maximum-xor-for-each-query/solutions/6021142/easiest-solution-beats-100-c-java-python3-javascript
+class Solution:
+    def getMaximumXor(self, nums: List[int], maximumBit: int) -> List[int]:
+        n = len(nums)
+        xorr = nums[0]
+        max_xor = (1 << maximumBit) - 1
+        
+        for i in range(1, n):
+            xorr ^= nums[i]
+        
+        ans = []
+        for i in range(n):
+            ans.append(xorr ^ max_xor)
+            xorr ^= nums[n - 1 - i]
+        
+        return ans
+
+        
