e97f1d249dc43eb50c0873988ac6b85d6842371a
Kunal Chawla <73349039+kcchawla85@users.noreply.github.com>
2024-11-27 20:50:02 +0100

    Create 27NOV_2024_3243_Shortest_Distance_After_Road_Addition_Queries_I.py

---
+++ 27NOV_2024_3243_Shortest_Distance_After_Road_Addition_Queries_I.py
@@ -0,0 +1,29 @@
+# https://leetcode.com/problems/shortest-distance-after-road-addition-queries-i/solutions/6087071/video-beats-100-explained-step-by-step
+class Solution:
+    def updateDistances(self, graph, current, distances):
+        newDist = distances[current] + 1
+        
+        for neighbor in graph[current]:
+            if distances[neighbor] <= newDist:
+                continue
+                
+            distances[neighbor] = newDist
+            self.updateDistances(graph, neighbor, distances)
+    
+    def shortestDistanceAfterQueries(self, n: int, queries: List[List[int]]) -> List[int]:
+        distances = [n - 1 - i for i in range(n)]
+        
+        graph = [[] for _ in range(n)]
+        for i in range(n-1):
+            graph[i + 1].append(i)
+        
+        answer = []
+        
+        for source, target in queries:
+            graph[target].append(source)
+            distances[source] = min(distances[source], distances[target] + 1)
+            self.updateDistances(graph, source, distances)
+            
+            answer.append(distances[0])
+        
+        return answer
